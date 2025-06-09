c6572ca333fdb695f4cad39f2a98c2eea2b1689d
Chayathon.T <gusgus157903@gmail.com>
2025-06-09 21:11:17 +0700

    Update Schema

---
+++ sql/admins.sql
@@ -0,0 +1,6 @@
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`AdminName`, `Password`, `isActive`, `CreatedAt`) VALUES
+('admin', '$2b$10$4.OAlaztF9R6.Vho4UJet.a.oujocS0BJtNzM4ZimYEBFNyL3PeEK', 1, '2025-06-04 14:43:10');
--- sql/users.sql
+++
@@ -1,6 +0,0 @@
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `role`, `isActive`, `createdAt`, `updatedAt`) VALUES
-(1, 'admin', '$2b$10$4.OAlaztF9R6.Vho4UJet.a.oujocS0BJtNzM4ZimYEBFNyL3PeEK', 'admin', 1, '2025-06-04 14:43:10', '2025-06-04 14:43:10');
